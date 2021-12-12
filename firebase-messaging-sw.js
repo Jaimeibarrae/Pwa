a362cc2d8cd164af505485c2f527df2d3c77ecfc
Jaimeibarrae <49220342+Jaimeibarrae@users.noreply.github.com>
2021-12-12 14:45:28 -0600

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,3 +13,13 @@ firebase.initializeApp(
       }
 )
 const messaging= firebase.messaging();
+ // Retrieve firebase messaging
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
