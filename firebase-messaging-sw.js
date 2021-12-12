1c05cd41003fcdc4d704b1170dd4ab3931145ae3
Jaimeibarrae <49220342+Jaimeibarrae@users.noreply.github.com>
2021-12-12 15:00:06 -0600

    Auto-generated commit [ci skip]

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,38 +14,24 @@ firebase.initializeApp(
 )
 const messaging= firebase.messaging();
  // Retrieve firebase messaging
-messaging.onBackgroundMessage(function(payload) {
-  console.log("Received background message ", payload);
+// Retrieve firebase messaging
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./firebase-messaging-sw.js")
-    .then(function(registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-      messaging.getToken({vapidKey: 'YOUR_VAPID_KEY', serviceWorkerRegistration : registration })
-        .then((currentToken) => {
-          if (currentToken) {
-            console.log('current token for client: ', currentToken);
-  
-            // Track the token -> client mapping, by sending to backend server
-            // show on the UI that permission is secured
-          } else {
-            console.log('No registration token available. Request permission to generate one.');
-  
-            // shows on the UI that permission is required 
-          }
-        }).catch((err) => {
-          console.log('An error occurred while retrieving token. ', err);
-          // catch error while creating client token
-        });  
+messaging.setBackgroundMessageHandler(function (payload) {
+   console.log('setBackgroundMessageHandler background message ', payload);
+
+   const promiseChain = clients
+      .matchAll({
+          type: "window",
+          includeUncontrolled: true
       })
-      .catch(function(err) {
-        console.log("Service worker registration failed, error:"  , err );
-    }); 
-  } 
+     .then(windowClients => {
+          for (let i = 0; i < windowClients.length; i++) {
+             const windowClient = windowClients[i];
+             windowClient.postMessage(payload);
+          }
+     })
+     .then(() => {
+          return self.registration.showNotification("my notification title");
+      });
+     return promiseChain;
+ });
